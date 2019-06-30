1ccb7c610a236840e404d043824b0617a65f743c
TheUberKid <zekixu@gmail.com>
2019-06-30 12:54:49 -0700

    made questions easier

--- includes/questions.js
+++ includes/questions.js
@@ -2,26 +2,31 @@ module.exports.math = [
   {
     text: 'George has 50 more apples than Gerald, who has half the apples of Frank. If Frank has 18 apples, how many apples does George have?',
     answer: 59,
-    duration: 30
+    duration: 40
   },
   {
     text: 'A triangle has a perimeter of 90 cm. The lengths of the three sides are in the ratio 5:12:13. What is the length of the shortest side in cm?',
     answer: 15,
-    duration: 45
+    duration: 60
   },
   {
     text: 'The distance around a rectangular garden is 36 feet. One side measures 10 feet. What is the area of the garden in feet?',
     answer: 80,
-    duration: 30
+    duration: 45
   },
   {
     text: 'Jason had $140 in his wallet. He spent 25% of his money at the bookstore. He then spent 20% of the remainder at Pizza Palace. How much money does Jason have left?',
     answer: 84,
-    duration: 60
+    duration: 90
   },
   {
     text: 'What is 6^2 + 5^2 + 4^2 + ... 1^2 ?',
     answer: 91,
+    duration: 60
+  },
+  {
+    text: 'If a bus has four people on it, and each person has five cats, how many legs are there on the bus?',
+    answer: 88,
     duration: 45
   }
 ]
