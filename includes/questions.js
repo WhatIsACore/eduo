c33b0ce1fcbc5a207bb4cef7c48eac839982aa57
TheUberKid <zekixu@gmail.com>
2019-06-30 12:33:53 -0700

    removed agora

--- includes/questions.js
+++ includes/questions.js
@@ -7,7 +7,7 @@ module.exports.math = [
   {
     text: 'A triangle has a perimeter of 90 cm. The lengths of the three sides are in the ratio 5:12:13. What is the length of the shortest side in cm?',
     answer: 15,
-    duration: 30
+    duration: 45
   },
   {
     text: 'The distance around a rectangular garden is 36 feet. One side measures 10 feet. What is the area of the garden in feet?',
@@ -17,11 +17,11 @@ module.exports.math = [
   {
     text: 'Jason had $140 in his wallet. He spent 25% of his money at the bookstore. He then spent 20% of the remainder at Pizza Palace. How much money does Jason have left?',
     answer: 84,
-    duration: 30
+    duration: 60
   },
   {
     text: 'What is 6^2 + 5^2 + 4^2 + ... 1^2 ?',
     answer: 91,
-    duration: 30
+    duration: 45
   }
 ]
